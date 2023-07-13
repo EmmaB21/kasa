ca861fdfa8f32154e75147d5e9180108034141ec
Emma B <emm2111@hotmail.fr>
2023-07-13 17:01:16 +0200

    Modif links header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,16 @@ import '../style/layout/header.css'
 function Header() {
     return (
         <div className='navbar'>
-            <img className= 'navbar__logo'src={logo} alt='Logo Kasa' />
+            <img className='navbar__logo' src={logo} alt='Logo Kasa' />
             <nav>
                 <ul className='navbar__links'>
-                <li><NavLink to="/">Accueil</NavLink></li>
-                <li><NavLink to="/about">A Propos</NavLink></li>
+                    <li><NavLink to="/" activeClassName="active">Accueil</NavLink></li>
+                    <li><NavLink to="/about" activeClassName="active">A Propos</NavLink></li>
                 </ul>
             </nav>
         </div>
     )
-    
+
 }
 
 export default Header
