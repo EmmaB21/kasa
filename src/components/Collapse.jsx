f7d5f28183633952a7cd8ed19f20de971ea25690
Emma B <emm2111@hotmail.fr>
2023-07-18 19:34:28 +0200

    page About

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import arrow_up from "../assets/arrow_up.png"
+import arrow_down from "../assets/arrow_down.png"
+import "../style/components/collapse.css"
+
+
+function Collapse({ title, text }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleComponentOpening = () => {
+        setIsOpen((open) => !open);
+    };
+    return (
+        <div className="collapse">
+            <div className='collapse__title__ctn'>
+                <h3 className='collapse__title'>{title}</h3>
+                <img onClick={handleComponentOpening} className="collapse__arrow"
+                    src={isOpen ? arrow_down : arrow_up} alt="flèche" />
+            </div>
+            <div className={`collapse__txt__ctn ${isOpen ? "" : "collapse__hiddenTxt"}`}>
+                <p className='collapse__txt'>{text}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Collapse
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,26 @@
-// import '../style/pages/home.css'
 import Banner from '../components/Banner'
 import img from '../assets/AboutBanner.jpg'
-import '../style/components/banner.css'
+import Collapse from '../components/Collapse'
+import "../style/pages/about.css"
 
 function About() {
     return (
         <div className='page__ctn'>
-            < Banner  image={img} ClassHeightBanner={"aboutBanner"} />
-            <h1>A propos</h1>
+            < Banner image={img} ClassHeightBanner={"aboutBanner"} />
+            <div className='collapses__ctn'>
+                <Collapse
+                    title="Fiabilité"
+                    text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." />
+                    <Collapse
+                    title="Respect"
+                    text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+                    <Collapse
+                    title="Service"
+                    text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+                    <Collapse
+                    title="Sécurité"
+                    text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+            </div>
         </div>
     )
 }
