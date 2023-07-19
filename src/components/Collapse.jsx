56ccc658b6143bc941ca2102ef380b3c421290c4
Emma B <emm2111@hotmail.fr>
2023-07-19 16:53:49 +0200

    Page 404

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -13,8 +13,10 @@ function Collapse({ title, text }) {
         <div className="collapse">
             <div className='collapse__title__ctn'>
                 <h3 className='collapse__title'>{title}</h3>
-                <img onClick={handleComponentOpening} className="collapse__arrow"
-                    src={isOpen ? arrow_down : arrow_up} alt="flèche" />
+                <img onClick={handleComponentOpening} 
+                className="collapse__arrow"
+                    src={isOpen ? arrow_down : arrow_up} alt="flèche"
+                     />
             </div>
             <div className={`collapse__txt__ctn ${isOpen ? "" : "collapse__hiddenTxt"}`}>
                 <p className='collapse__txt'>{text}</p>
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,14 @@
+import "../style/pages/error.css"
+
 function Error() {
     return (
-        <div>
-            <h1>OUPS</h1>
+        <div className="page__ctn error">
+            <h1 className="error__title">404</h1>
+            <p className="error__txt">Oups! La page que vous demandez n'existe pas.</p>
+            <a className="error__link" href="/">Retourner sur la page d'accueil
+            </a>
         </div>
+
     )
 }
 
