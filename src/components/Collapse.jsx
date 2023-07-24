a68c5730b75a3101f67ce7e4514e37054d1e5c22
Emma B <emm2111@hotmail.fr>
2023-07-24 12:11:00 +0200

    Ajout anim flèche collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import arrow_up from "../assets/arrow_up.png"
-import arrow_down from "../assets/arrow_down.png"
+// import arrow_down from "../assets/arrow_down.png"
 import "../style/components/collapse.css"
 
 
@@ -14,8 +14,8 @@ function Collapse({ title, text, ClassAccomodationCollapse }) {
             <div className='collapse__title__ctn'>
                 <h3 className='collapse__title'>{title}</h3>
                 <img onClick={handleComponentOpening}
-                    className="collapse__arrow"
-                    src={isOpen ? arrow_down : arrow_up} alt="flèche"
+                    className={`"collapse__arrow ${isOpen ? "arrow_down" : "arrow_up"}`}
+                    src={arrow_up} alt="flèche"
                 />
             </div>
             <div className={`collapse__txt__ctn ${isOpen ? "" : "collapse__hiddenTxt"}`}>
